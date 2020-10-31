f4c8b2e56146f9aa291f774dfe54c1451de0ec0f
patil-pooja28 <pooja.patil@spit.ac.in>
2020-10-31 13:24:47 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { InfopageComponent } from './infopage/infopage.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"", component:HomeComponent
+  },
+  {
+    path:"home", component:HomeComponent
+  },
+  {
+    path:"aboutus", component:AboutusComponent
+  },
+  {
+    path:"infopage", component:InfopageComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
