090e629579c31c5442e161e34fed04a99e14e1de
Neha Rane <neharane2504@gmail.com>
2020-11-03 12:47:10 +0530

    adding api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { InfopageComponent } from './infopage/infopage.component';
 import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,8 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
+    FormsModule,
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  serverData: JSON;
+  employeeData: JSON;
+  keyword;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+  
+  nextPage(key) {
+    console.log(key)
+    this.httpClient.post('http://127.0.0.1:5000/search',key).subscribe(data => {
+      //this.serverData = data as JSON;
+      console.log(data);
+    })
+  }
+  
 
   ngOnInit(): void {
+    
   }
+
+
 
 }
